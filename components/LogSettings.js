24214370c991bd3d8fd534323e2feb8f5ab0b379
Cory Ellerbroek <cory.ellerbroek@gmail.com>
2021-10-12 15:35:29 -0500

    Submit button logs state object

--- components/LogSettings.js
+++ components/LogSettings.js
@@ -8,69 +8,136 @@ import {
   Button,
   Alert,
 } from 'react-native';
-import {Picker} from '@react-native-picker/picker';
-import LocationPicker from './LocationPicker';
+// import {Picker} from '@react-native-picker/picker';
+// import LocationPicker from './LocationPicker';
 
 const LogSettings = () => {
-  const [dateText, onChangeDateText] = useState('');
-  const [selectedLocation, setSelectedLocation] = useState();
+  const [date, onChangeDate] = useState('');
+  const [location, onChangeLocation] = useState('');
+  const [bike, onChangeBike] = useState('');
+  const [forkPSI, onChangeForkPSI] = useState(null);
+  const [forkCompression, onChangeForkCompression] = useState(null);
+  const [forkRebound, onChangeForkRebound] = useState(null);
+  const [shockPSI, onChangeShockPSI] = useState(null);
+  const [shockCompression, onChangeShockCompression] = useState(null);
+  const [shockRebound, onChangeShockRebound] = useState(null);
+  const [frontTirePSI, onChangeFrontTirePSI] = useState(null);
+  const [rearTirePSI, onChangeRearTirePSI] = useState(null);
+
+  // const [selectedLocation, setSelectedLocation] = useState();
+
+  const handleClick = () => {
+    console.log({
+      date: date,
+      location: location,
+      bike: bike,
+      forkPSI: forkPSI,
+      forkCompression: forkCompression,
+      forkRebound: forkRebound,
+      shockPSI: shockPSI,
+      shockCompression: shockCompression,
+      shockRebound: shockRebound,
+      frontTirePSI: frontTirePSI,
+      rearTirePSI: rearTirePSI,
+    });
+  };
 
   return (
     <View>
-      <Text style={styles.title}>Log Setting</Text>
+      <Text style={styles.title}>Log Settings</Text>
       <Text style={styles.entryName}>Date</Text>
-      <TextInput style={styles.input} placeholder="Date"></TextInput>
+      <TextInput
+        style={styles.input}
+        placeholder="Date"
+        onChangeText={onChangeDate}></TextInput>
 
       {/* <LocationPicker
         selectedLocation={selectedLocation}
         setSelectedLocation={setSelectedLocation}
       /> */}
       <Text style={styles.entryName}>Location</Text>
-      <TextInput style={styles.input} placeholder="Location"></TextInput>
+      <TextInput
+        style={styles.input}
+        placeholder="Location"
+        // value={values.location}
+        onChangeText={onChangeLocation}></TextInput>
       <Text style={styles.entryName}>Bike</Text>
-      <TextInput style={styles.input} placeholder="Bike"></TextInput>
+      <TextInput
+        style={styles.input}
+        placeholder="Bike"
+        onChangeText={onChangeBike}></TextInput>
       <Text style={styles.title}>Fork</Text>
       <View style={styles.container}>
         <View style={styles.thirdInput}>
           <Text style={styles.entryName}>PSI</Text>
-          <TextInput style={styles.input} placeholder="PSI"></TextInput>
+          <TextInput
+            style={styles.input}
+            onChangeText={onChangeForkPSI}
+            placeholder="PSI"
+            value={forkPSI}
+            keyboardType="numeric"></TextInput>
         </View>
         <View style={styles.thirdInput}>
           <Text style={styles.entryName}>Compression</Text>
-          <TextInput style={styles.input} placeholder="Compression"></TextInput>
+          <TextInput
+            style={styles.input}
+            placeholder="Compression"
+            onChangeText={onChangeForkCompression}
+            value={forkCompression}
+            keyboardType="numeric"></TextInput>
         </View>
         <View style={styles.thirdInput}>
           <Text style={styles.entryName}>Rebound</Text>
-          <TextInput style={styles.input} placeholder="Rebound"></TextInput>
+          <TextInput
+            style={styles.input}
+            placeholder="Rebound"
+            onChangeText={onChangeForkRebound}
+            value={forkRebound}
+            keyboardType="numeric"></TextInput>
         </View>
       </View>
       <Text style={styles.title}>Shock</Text>
       <View style={styles.container}>
         <View style={styles.thirdInput}>
           <Text style={styles.entryName}>PSI</Text>
-          <TextInput style={styles.input} placeholder="PSI"></TextInput>
+          <TextInput
+            style={styles.input}
+            placeholder="PSI"
+            onChangeText={onChangeShockPSI}></TextInput>
         </View>
         <View style={styles.thirdInput}>
           <Text style={styles.entryName}>Compression</Text>
-          <TextInput style={styles.input} placeholder="Compression"></TextInput>
+          <TextInput
+            style={styles.input}
+            placeholder="Compression"
+            onChangeText={onChangeShockCompression}></TextInput>
         </View>
         <View style={styles.thirdInput}>
           <Text style={styles.entryName}>Rebound</Text>
-          <TextInput style={styles.input} placeholder="Rebound"></TextInput>
+          <TextInput
+            style={styles.input}
+            placeholder="Rebound"
+            onChangeText={onChangeShockRebound}></TextInput>
         </View>
       </View>
       <Text style={styles.title}>Tires</Text>
       <View style={styles.container}>
         <View style={styles.halfInput}>
           <Text style={styles.entryName}>Front PSI</Text>
-          <TextInput style={styles.input} placeholder="Front PSI"></TextInput>
+          <TextInput
+            style={styles.input}
+            placeholder="Front PSI"
+            onChangeText={onChangeFrontTirePSI}></TextInput>
         </View>
         <View style={styles.halfInput}>
           <Text style={styles.entryName}>Rear PSI</Text>
-          <TextInput style={styles.input} placeholder="Rear PSI"></TextInput>
+          <TextInput
+            style={styles.input}
+            placeholder="Rear PSI"
+            onChangeText={onChangeRearTirePSI}></TextInput>
         </View>
       </View>
-      <Button title="Submit" />
+      <Button title="Submit" onPress={handleClick} />
     </View>
   );
 };
