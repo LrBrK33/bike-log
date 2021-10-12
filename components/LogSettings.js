fe074cd6841bbc5d358349b2c242d712ca4d3ff0
Cory Ellerbroek <cory.ellerbroek@gmail.com>
2021-10-12 12:17:20 -0500

    Adds component files

---
+++ components/LogSettings.js
@@ -0,0 +1,12 @@
+import React, {useState} from 'react';
+import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
+
+const LogSettings = () => {
+  return (
+    <View>
+      <Text>Log Settings here</Text>
+    </View>
+  );
+};
+
+export default LogSettings;
