36506fd84eeafdef33f9d1e22405510f3ff8f062
Cory Ellerbroek <cory.ellerbroek@gmail.com>
2021-10-13 14:01:03 -0500

    Basic db setup

---
+++ db/index.js
@@ -0,0 +1,11 @@
+var mysql = require('mysql');
+var connection = mysql.createConnection();
+
+connection.connect();
+
+connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+  if (error) throw error;
+  console.log('The solution is: ', results[0].solution);
+});
+
+connection.end();
